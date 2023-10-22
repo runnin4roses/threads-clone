5bbde6b03c768710c023e3a4f49806e02c1bac13
Gillian Ayers <runnin4roses@gmail.com>
2023-10-22 11:09:27 -0700

    Hooked up basic user data from json server

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import './index.css';
 import Nav from './components/Nav';
 import Header from './components/Header';
@@ -5,14 +6,48 @@ import Feed from './components/Feed';
 import Popup from './components/Popup';
 
 const App = () => {
+  const [user, setUser] = useState(null);
+  const [threads, setThreads] = useState(null);
+  const userId = "44d3dc3d-d7c8-4207-bcff-f58df362a641";
+
+  const getUser = async () => {
+    try {
+      const response = await fetch(`http://localhost:3000/users?user_uuid=${userId}`);
+      const data = await response.json();
+      setUser(data[0]);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  const getThreads = async () => {
+    try {
+      const response = await fetch(`http://localhost:3000/threads?thread_from=${userId}`);
+      const data = await response.json();
+      setThreads(data);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  useEffect(() => {
+    getUser();
+    getThreads();
+  }, [])
+
+  console.log(threads);
 
   return (
-    <div className="app">
-      <Nav />
-      <Header />
-      <Feed />
-      {/* <Popup /> */}
-    </div>
+    <>
+      {user && (
+        <div className="app">
+          <Nav url={user.instagram_url} />
+          <Header user={user} />
+          <Feed />
+          {/* <Popup /> */}
+        </div>)
+      }
+    </>
   )
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,19 @@
-const Header = () => {
+const Header = ({ user }) => {
 
     return (
         <header>
             <div className="info-container">
                 <div className="user-info-container">
-                    <h1>username</h1>
-                    <p>handle <span className="threads-info">threads.net</span></p>
+                    <h1>{user.username}</h1>
+                    <p>{user.handle}<span className="threads-info">threads.net</span></p>
                 </div>
                 <div className="img-container">
-                    <img src="" alt="profile avatar" />
+                    <img src={user.img} alt="profile avatar" />
                 </div>
             </div>
-            <p>bio</p>
+            <p>{user.bio}</p>
             <div className="sub-info-container">
-                <p className="sub-text">X followers • <a href="">link</a></p>
+                <p className="sub-text">{user.followers.length} followers • <a href={user.link}>{user.link.replace('https://www.', '')}</a></p>
             </div>
             <button className="primary" onClick={() => navigator.clipboard.writeText('I am a URL')}>Share Profile</button>
             <div className="button-container">
